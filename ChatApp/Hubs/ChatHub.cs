e9b4873109bae051500145f7601b10c67b285ab0
Ahmadmmasri <xxmen-95@hotmail.com>
2021-11-18 14:53:07 +0200

    Add project files.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub :Hub
+    {
+        //send message (Ahmad,Hi there)
+        public async Task SendMessage(string fromUser, string Messgae)
+        {
+            //send for all users
+            await Clients.All.SendAsync("RecieveMessage", fromUser, Messgae);
+        } 
+    }
+}
---
+++ ChatApp/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿
+//create connection from ChatHub
+var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
+//add data to list 
+connection.on("RecieveMessage", function (fromUser, Message) {
+    var msg = fromUser + ": " + Message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li)
+});
+
+//start 
+connection.start();
+
+//take messages and send it to SendMessage method in ChatHub class
+$("#btnSend").on("click", function () {
+    var fromUser = $("#textUser").val();
+    var Message = document.getElementById("textMessage").value;
+    console.log("hi",Message)
+
+    connection.invoke("SendMessage", fromUser, Message)
+});
